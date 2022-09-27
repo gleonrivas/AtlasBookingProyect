e6b9300d319fd46aa5495097c6cf219e8b855b11
gonzalo <gleonrivas@safareyes.es>
2022-09-27 19:12:42 +0200

    se sube SRC, TEST, POM.

---
+++ src/main/java/com/app/atlasultimate/AtlasUltimateApplication.java
@@ -0,0 +1,13 @@
+package com.app.atlasultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasUltimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlasUltimateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/atlasultimate/AtlasUltimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.atlasultimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasUltimateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
