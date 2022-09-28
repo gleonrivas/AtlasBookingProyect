c894124471c740c2c19929f15733280ac5c2465b
lortegaperal <lortegaperal@safareyes.es>
2022-09-28 20:15:17 +0200

    creacion de modelos

--- src/main/java/com/app/atlasultimate/model/tarifas.java
+++ src/main/java/com/app/atlasultimate/model/tarifas.java
@@ -1,4 +1,21 @@
 package com.app.atlasultimate.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tarifas")
+@Getter
+@Setter
+@EqualsAndHashCode
 public class tarifas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "baño_habitacion")
 }
