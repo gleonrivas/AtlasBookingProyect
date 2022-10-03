bf7dc58ca3e9b72b6e4fac6fc8ec86e2d788f4a7
celim <celimva@gmail.com>
2022-10-03 19:06:03 +0200

    localhost index

--- src/main/java/com/app/atlasultimate/AtlasUltimateApplication.java
+++ src/main/java/com/app/atlasultimate/AtlasUltimateApplication.java
@@ -3,13 +3,13 @@ package com.app.atlasultimate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class AtlasUltimateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AtlasUltimateApplication.class, args);
-
     }
 
 }
--- src/main/java/com/app/atlasultimate/controller/AdministradorController.java
+++ src/main/java/com/app/atlasultimate/controller/AdministradorController.java
@@ -3,18 +3,27 @@ package com.app.atlasultimate.controller;
 import com.app.atlasultimate.model.Administrador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RestController
+@RequestMapping("/Admin")
 public class AdministradorController {
-    @GetMapping("/index")
-    public String showForm(Model model) {
-        Administrador nuevo = new Administrador();
-        model.addAttribute("nuevo", nuevo);
-
-
-        return "RegistroInicioSesionHotel";
+    @RequestMapping("/RegistroAdministrador")
+    public String showUserForm(Model model){
+        model.addAttribute("administrador", new Administrador());
+        return "/RegistroeInicioSesionHotel.html";
     }
-}
+    @RequestMapping("/create")
+    public ModelAndView createUser(Administrador admin, BindingResult result) {
+        ModelAndView model = new ModelAndView();
+        model.addObject("administrador", admin);
+        model.setViewName(result.hasErrors() ? "userForm" : "userReady");
+        return model;
+    }
+
+    }
