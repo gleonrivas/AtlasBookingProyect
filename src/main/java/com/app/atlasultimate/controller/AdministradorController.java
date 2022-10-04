8d2b248d3ac87f88023540eac512677cfa06360e
celim <celimva@gmail.com>
2022-10-04 17:23:39 +0200

    controller y html

--- src/main/java/com/app/atlasultimate/controller/AdministradorController.java
+++ src/main/java/com/app/atlasultimate/controller/AdministradorController.java
@@ -6,24 +6,44 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RestController
-@RequestMapping("/Admin")
+@RequestMapping("Admin")
 public class AdministradorController {
-    @RequestMapping("/RegistroAdministrador")
-    public String showUserForm(Model model){
-        model.addAttribute("administrador", new Administrador());
-        return "/RegistroeInicioSesionHotel.html";
-    }
-    @RequestMapping("/create")
-    public ModelAndView createUser(Administrador admin, BindingResult result) {
-        ModelAndView model = new ModelAndView();
-        model.addObject("administrador", admin);
-        model.setViewName(result.hasErrors() ? "userForm" : "userReady");
-        return model;
+
+    @GetMapping("Inicio")
+    public String inicio(){
+        return "/InicioAdministrador.html";
     }
 
+    @GetMapping("Servicios")
+    public String servs(){
+        return "/ofertas_servicios_admin.html";
+    }
+
+    @GetMapping("Informes")
+    public String informes(){
+        return "/InformesAdmin.html";
+    }
+    @GetMapping("Facturas")
+    public String facturas(){
+        return "/FacturasAdmin.html";
+    }
+
+    @GetMapping("Registro")
+    public String regis(Model model){
+        model.addAttribute("administrador", new Administrador());
+        return "/registro_admin.html";
+    }
+
+    @RequestMapping(value="/crear", method= RequestMethod.POST)
+    public ModelAndView form(Administrador administrador){
+        ModelAndView model = new ModelAndView();
+        model.addObject("administrador", administrador);
+        return model;
+
+    }
     }
