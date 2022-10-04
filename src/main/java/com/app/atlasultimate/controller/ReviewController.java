3e3796b8aea11b5c35e7db25aebcef791d3d8066
celim <celimva@gmail.com>
2022-10-04 10:38:07 +0200

    aprendiendo el controller con comentarios admin

--- src/main/java/com/app/atlasultimate/controller/ReviewController.java
+++ src/main/java/com/app/atlasultimate/controller/ReviewController.java
@@ -1,9 +1,15 @@
 package com.app.atlasultimate.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
+@RequestMapping("review")
 public class ReviewController {
+    @GetMapping("create")
+    public String create(){
+        return "/ComentariosAdmin.html";
+    }
 }
