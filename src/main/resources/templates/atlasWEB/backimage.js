b4343aa734a27854549cc7cc7c2ea91c3c1d3475
gonzalo <gleonrivas@safareyes.es>
2022-10-04 21:10:30 +0200

    Actualizacion caos.

---
+++ src/main/resources/templates/atlasWEB/backimage.js
@@ -0,0 +1,17 @@
+const imagenes = [
+    "/templates/atlasWEB/img/nyk-wallpaper.jpeg",
+    "/templates/atlasWEB/img/venecia.jpg",
+    "/templates/atlasWEB/img/paris.jpg"
+    ];
+
+function sleep(milliseconds) {
+    let start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}
+
+
+document.body.style.backgroundImage = "url('https://sebhastian.com/img/default.png')";
