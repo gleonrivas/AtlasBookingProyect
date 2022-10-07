8abd4764eed6344f361e84390b2d345b8b41c55f
lortegaperal <lortegaperal@safareyes.es>
2022-10-07 18:41:51 +0200

    Empezando la pagina del hotel

--- src/main/java/com/app/atlasultimate/registro/ClienteRepository.java
+++ src/main/java/com/app/atlasultimate/registro/ClienteRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+    public Cliente findByEmail(String email);
 
 }
