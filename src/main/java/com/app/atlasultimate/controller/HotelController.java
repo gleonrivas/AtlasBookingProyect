af9f732f94ba65feef221478c822c1ef163a1117
lortegaperal <lortegaperal@safareyes.es>
2022-10-11 20:05:01 +0200

    cambios en clases hotel y habitacion

--- src/main/java/com/app/atlasultimate/controller/HotelController.java
+++ src/main/java/com/app/atlasultimate/controller/HotelController.java
@@ -1,9 +1,14 @@
 package com.app.atlasultimate.controller;
 
+import com.app.atlasultimate.service.HotelService;
+import com.app.atlasultimate.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController
 public class HotelController {
+
 }
