609de568ff60fe995022a06fceac7f843de5451f
gonzalo <gleonrivas@safareyes.es>
2022-10-17 02:35:22 +0200

    ajuste minimo y backup.

--- src/main/java/com/app/atlasultimate/repository/HabitacionRepository.java
+++ src/main/java/com/app/atlasultimate/repository/HabitacionRepository.java
@@ -2,7 +2,9 @@ package com.app.atlasultimate.repository;
 
 import com.app.atlasultimate.model.Habitacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HabitacionRepository  extends JpaRepository<Habitacion,Integer> {
 
 }
