3ea946873b6ac358ffa3df8aa5f65c61019bfd41
lortegaperal <lortegaperal@safareyes.es>
2022-11-09 19:07:59 +0100

    crear comentarios realizado

--- src/main/java/com/app/atlasultimate/repository/ReviewRepository.java
+++ src/main/java/com/app/atlasultimate/repository/ReviewRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
-    @Query(value = "select * from review where id_hotel = :id_hotel limit 10", nativeQuery = true)
+    @Query(value = "select * from review where id_hotel = :id_hotel", nativeQuery = true)
     List<Review> find10LastValues(@Param("id_hotel")Integer id_hotel);
 
     List<Review> findAllByHotel(Hotel hotel);
