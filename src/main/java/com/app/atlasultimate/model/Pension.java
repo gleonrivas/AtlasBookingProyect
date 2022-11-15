16472784093f93e743229cd519647ec79b8d9cff
gonzalo <gleonrivas@safareyes.es>
2022-11-16 00:45:03 +0100

    eliminado many to many

--- src/main/java/com/app/atlasultimate/model/Pension.java
+++ src/main/java/com/app/atlasultimate/model/Pension.java
@@ -52,9 +52,6 @@ public class Pension {
     @JoinColumn(name = "id_hotel")
     private Hotel id_hotel;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    private List<Hotel> hotel;
-
     public Pension(Double sa, Boolean booleanSa, Double ad, Boolean booleanAd, Double mp, Boolean booleanMp, Double pc, Boolean booleanPc, Hotel id_hotel) {
         this.sa = sa;
         this.booleanSa = booleanSa;
