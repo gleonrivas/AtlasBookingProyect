829d97f994a4af007e7adf4756ed58464549ecfe
celim <celimva@gmail.com>
2022-11-23 18:51:49 +0100

    guardar hotel hecho, pero problema del Autowired

---
+++ src/test/java/com/app/atlasultimate/Buscar/PruebaBuscarRepository.java
@@ -0,0 +1,5 @@
+package com.app.atlasultimate.Buscar;
+
+public class PruebaBuscarRepository {
+
+}
--- src/test/java/com/app/atlasultimate/Hoteles/PruebaHotelRepository.java
+++ src/test/java/com/app/atlasultimate/Hoteles/PruebaHotelRepository.java
@@ -7,10 +7,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import static org.springframework.test.util.AssertionErrors.*;
 
 
@@ -23,18 +29,13 @@ import static org.springframework.test.util.AssertionErrors.*;
         "spring.jpa.properties.hibernate.default_schema=",
         "spring.jpa.hibernate.ddl-auto=update",
 })
-
+@TestExecutionListeners
+@ContextConfiguration(classes = HotelRepository.class)
 public class PruebaHotelRepository {
 
-
+    @Autowired
     HotelRepository hotelRepository;
 
-    private TestEntityManager entityManager;
-
-
-    @BeforeEach
-
-    @AfterEach
 
     @Test
     public void guardarHoteslTest(){
@@ -47,6 +48,7 @@ public class PruebaHotelRepository {
         //Then
         assertNotNull("No se ha guardado el hotel en base de datos",hotelEsperado);
         assertEquals("Los hoteles no coinciden",hotelEsperado.getNombre(), hotelObtenido.getNombre());
+
 
     }
 }
