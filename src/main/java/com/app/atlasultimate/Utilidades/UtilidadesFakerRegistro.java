f3de7c1f9f39d5a160481f0edff0513ec6fba11d
gonzalo <gleonrivas@safareyes.es>
2022-11-29 06:52:53 +0100

    Faker registro

---
+++ src/main/java/com/app/atlasultimate/Utilidades/UtilidadesFakerRegistro.java
@@ -0,0 +1,26 @@
+package com.app.atlasultimate.Utilidades;
+
+import com.app.atlasultimate.model.Registro;
+import com.app.atlasultimate.model.tipo_pago;
+import com.app.atlasultimate.model.tipo_pension;
+
+public class UtilidadesFakerRegistro {
+
+    public static Registro crearRegistroTest(){
+
+        return new Registro(
+                "2023-01-01",
+                "2023-01-10",
+                1,
+                tipo_pago.tarjeta,
+                tipo_pension.ad,
+                10.0,
+                1,
+                UtilidadesFakerUsuario.crearUsuario(),
+                UtilidadesFakerHabitacion.crearHabitacion(),
+                true
+        );
+
+    }
+
+}
