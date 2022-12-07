ad93074b84e0b40a2ba0f79b02a3a8a159f3a5c0
gonzalo <gleonrivas@safareyes.es>
2022-12-07 05:57:51 +0100

    eliminado tipos pension gonzalo

--- src/main/java/com/app/atlasultimate/model/tipo_pension.java
+++ src/main/java/com/app/atlasultimate/model/tipo_pension.java
@@ -1,19 +1,5 @@
 package com.app.atlasultimate.model;
 
 public enum tipo_pension {
-    sa("Solo alojamineto"),
-    ad("Almuerzo y desayuno"),
-    pc("Pensión completa"),
-    mp("Media pensión");
-
-    private final String displayValue;
-
-    tipo_pension(String displayValue) {
-        this.displayValue = displayValue;
-    }
-
-    public String getDisplayValue() {
-        return displayValue;
-    }
-
+    sa, ad, pc, mp
 }
